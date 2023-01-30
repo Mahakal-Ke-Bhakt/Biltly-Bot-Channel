7431bb53b31ae74b960c4d81a78af45961a9d553
Mahakal-Ke-Bhakt <123706567+Mahakal-Ke-Bhakt@users.noreply.github.com>
2023-01-30 17:04:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 from pyrogram import Client
 
-API_ID = int(os.environ.get("API_ID") or 0)
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID", "10683462"))
+API_HASH = os.environ.get("API_HASH", "8ab812d6e6849bd6352dcb731e44c31e")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5680348811:AAFTg0TmzNx6dBPG5A2TQ3meJq3_npGVs2A")
 
 SEND_TEXT = """ Hello {},
-`I am Bitly Bot to shorten links.
-Send a valid link to shorten.`
+I am Bitly Bot to shorten links.
+Send a valid link to shorten.
 
-Deployed with ❤️ By @GreyMatter_Bots
+Deployed with ❤️ By @DKBOTZ
 """
 
 bot = Client(
